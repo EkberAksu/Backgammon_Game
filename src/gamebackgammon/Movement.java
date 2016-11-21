bd9796fba0182454e71235680730237c0c35b941
Soner "Jack <sonerakarrr@gmail.com>
2016-11-21 23:45:14 +0300

    The class for movement with GUI for two players

---
+++ src/gamebackgammon/Movement.java
@@ -0,0 +1,39 @@
+package gamebackgammon;
+
+import java.util.ArrayList;
+
+/**
+  * Soner
+ */
+public class Movement {
+    char character;
+    int now;
+    int after;
+    int point;
+    
+    
+    Movement(int now, int after,char character){
+        this.now = now;
+        this.after = after;
+        this.character = character;
+    }
+    Movement(){
+        this.now = -1;
+        this.after = -1;
+        this.character = 'N';
+    }
+    void setMoveChar(char ch) { character = ch; }   
+    void setMoveNow(int nowTemp) { now = nowTemp; }
+    void setMoveAfter(int afterTemp) { after = afterTemp; }  
+    void setPoint(int point) { this.point = point; }
+    char getMoveChar() { return character; }
+    int getMoveNow() { return now; }   
+    int getMoveAfter() { return after; }
+    int getPoint() { return point; }
+    void addPoint(int point) {this.point += point; }
+    
+    @Override
+    public String toString(){
+        return getMoveChar() + ":" + getMoveNow()+"  = > " + getMoveAfter() + " puan: " +getPoint();
+    }
+}
