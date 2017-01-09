572611f300e95af47e063fb3560dd8d2f7329034
slymnbalaban <blbnSlymn54@gmail.com>
2017-01-09 03:38:47 +0300

    change somethings for database(old game save)

--- src/gamebackgammon/Manual.java
+++ src/gamebackgammon/Manual.java
@@ -1,9 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package gamebackgammon;
 
 import static gamebackgammon.Artificial.game1;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 
 public class Manual extends javax.swing.JFrame {
     static Backgammon game = new Backgammon("isim",'X');
